795a2df7ca2b28e17060f108883227aa33ad7bd0
BaltaMolteni <baltazarmolteni04@gmail.com>
2025-06-03 17:15:47 -0300

    implementacion recetas favoritas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ const ThemedStatusBar = () => {
   const { theme, themeName } = useTheme();
   return (
     <StatusBar
-      barStyle={themeName === 'light' ? 'light-content' : 'dark-content'}
+      barStyle={themeName === 'light' ? 'dark-content' : 'light-content'}
       backgroundColor={theme.surface} 
     />
   );
@@ -18,7 +18,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider>
       <ThemedStatusBar />
-      <Stack/>
+      <Stack screenOptions={{ headerShown: false }}/>
     </ThemeProvider>
   );
 }
